a28704a1a8ae2c06a1a18c82f11defbcf78e8903
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-05-17 10:27:29 -0400

    updated passport file from js missing return error

--- passport.js
+++ passport.js
@@ -7,35 +7,49 @@ const Users = Models.User;
 const jwtStrategy = passportJWT.Strategy;
 const extractJWT = passportJWT.extractJWT;
 
-passport.use(new localStrategy( {
-	usernameField: 'Username',
-	passwordField: 'Password'
-}, (username, password, callback) => {
-	console.log(username + ' ' + password);
-	Users,findOne({ Username: username }, (error, user) => {
-		if (error) {
-			console.log(error);
-			return callback(error);
-		}
+passport.use(
+  new localStrategy(
+    {
+      usernameField: 'Username',
+      passwordField: 'Password'
+    },
+    (username, password, callback) => {
+      console.log(username + ' ' + password);
+      Users,
+        findOne({ Username: username }, (error, user) => {
+          if (error) {
+            console.log(error);
+            return callback(error);
+          }
 
-		if(!user) {
-			console.log('incorrect username');
-			return callback(null, false, {message: 'Incorrect username or password.'});
-		}
+          if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {
+              message: 'Incorrect username or password.'
+            });
+          }
 
-		console.log('finished');
-		return callback(null, user);
-	});
-}));
+          console.log('finished');
+          return callback(null, user);
+        });
+    }
+  )
+);
 
-passport.use(new jwtStrategy({
-	jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
-	secretOrKey: 'your_jwt_secret'
-}, (jwtPayload, callback) => {
-	.then((user) => {
-		return callback(null, user);
-	})
-	.catch((error) => {
-		return callback(error)
-	});
-}));
+passport.use(
+  new jwtStrategy(
+    {
+      jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'your_jwt_secret'
+    },
+    (jwtPayload, callback) => {
+      return Users.findById(jwtPayload._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
+    }
+  )
+);
