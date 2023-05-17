160361212fb3f43a039d912854c72e137fb0c5dd
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-05-17 10:24:26 -0400

    added passport strategies for user authentication and jwt authentication

---
+++ passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const Models = require('./models.js');
+const passportJWT = require('passport-jwt');
+
+const Users = Models.User;
+const jwtStrategy = passportJWT.Strategy;
+const extractJWT = passportJWT.extractJWT;
+
+passport.use(new localStrategy( {
+	usernameField: 'Username',
+	passwordField: 'Password'
+}, (username, password, callback) => {
+	console.log(username + ' ' + password);
+	Users,findOne({ Username: username }, (error, user) => {
+		if (error) {
+			console.log(error);
+			return callback(error);
+		}
+
+		if(!user) {
+			console.log('incorrect username');
+			return callback(null, false, {message: 'Incorrect username or password.'});
+		}
+
+		console.log('finished');
+		return callback(null, user);
+	});
+}));
+
+passport.use(new jwtStrategy({
+	jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
+	secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+	.then((user) => {
+		return callback(null, user);
+	})
+	.catch((error) => {
+		return callback(error)
+	});
+}));
