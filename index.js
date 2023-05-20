9897b33514dda43437ce8217c8c139a2361966cb
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-05-20 13:05:15 -0400

    updated port from 8080

--- index.js
+++ index.js
@@ -311,6 +311,7 @@ app.use((err, req, res, next) => {
 });
 
 //if everything functions correctly this message is logged from port 8080 thats listening.
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0,0,0,0', () => {
+  console.log('Listening on Port.');
 });
