9cc51b524bedc752b23c80a71b1d98f5407fd005
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-06-30 08:56:32 -0400

    updated error for user doesnt exist

--- index.js
+++ index.js
@@ -362,7 +362,7 @@ app.post('/forgot-password', async (req, res) => {
     //variable to find user by email that forgot password
     const oldUser = await Users.findOne({ Email });
     if (!oldUser) {
-      return res.json({ status: 'User Not Exists!' });
+      return res.status(404);
     }
     //created secret token that users get by email that expires in 5m
     const secret = JWT_SECRET + oldUser.Password;
